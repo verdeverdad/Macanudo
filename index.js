ea720242a36bb2a92456e6d53635bb5076ae961a
verdeverdad <sofia.16d@gmail.com>
2022-10-24 21:02:36 -0300

    js

---
+++ index.js
@@ -0,0 +1,25 @@
+window.addEventListener('scroll', function()  {
+    let element = document.getElementById('scroll-content');
+    let screenSize = window.innerHeight;
+    
+      if(element.getBoundingClientRect().top < screenSize) {
+        element.classList.add('visible');
+      } else {
+        element.classList.remove('visible');
+      }
+  });
+
+  window.addEventListener('scroll', function()  {
+    let elements = document.getElementsByClassName('scroll-content');
+    let screenSize = window.innerHeight;
+    
+      for(var i = 0; i < elements.length; i++) {
+        let element = elements[i];
+  
+        if(element.getBoundingClientRect().top < screenSize) {
+          element.classList.add('visible');
+        } else {
+          element.classList.remove('visible');
+        }
+      }
+  });
